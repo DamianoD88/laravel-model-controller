bafde05daf45ac3163e7024c4d69c117eaf92276
DamianoD88 <dispe88@live.it>
2021-09-07 17:33:10 +0200

    add views e royte

---
+++ resources/views/home.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        
+    </head>
+    <body>
+       
+        <div class="links">
+            <a href="{{ route('caratteri')}}">CHARACTERS</a>
+            <a href="{{ route('fumetti')}}">COMICS</a>
+            <a href="{{ route('films')}}">MOVIES</a>
+            <a href="{{ route('television')}}">TV</a>
+            <a href="{{ route('giochi')}}">GAMES</a>
+            <a href="{{ route('collezionabili')}}">COLLECTIBLES</a>
+            <a href="{{ route('video')}}">VIDEOS</a>
+            <a href="{{ route('supporters')}}">FANS</a>
+            <a href="{{ route('novità')}}">NEWS</a>
+            <a href="{{ route('negozio')}}">SHOP</a>
+        </div>
+          
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,45 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('homepage');
+
+Route::get('/caratteri', function () {
+    return view('CHARACTERS');
+})->name('caratteri');
+
+Route::get('/fumetti', function () {
+    return view('COMICS');
+})->name('fumetti');
+
+Route::get('/films', function () {
+    return view('MOVIES');
+})->name('films');
+
+Route::get('/television', function () {
+    return view('TV');
+})->name('television');
+
+Route::get('/giochi', function () {
+    return view('GAMES');
+})->name('giochi');
+
+Route::get('/collezionabili', function () {
+    return view('COLLECTIBLES');
+})->name('collezionabili');
+
+Route::get('/video', function () {
+    return view('VIDEOS');
+})->name('video');
+
+Route::get('/supporters', function () {
+    return view('FANS');
+})->name('supporters');
+
+Route::get('/novità', function () {
+    return view('NEWS');
+})->name('novità');
+
+Route::get('/negozio', function () {
+    return view('SHOP');
+})->name('negozio');
