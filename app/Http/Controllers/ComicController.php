5073e6627ddf8eea26de7ce8422f96c1d482e5b6
DamianoD88 <dispe88@live.it>
2021-09-08 00:22:33 +0200

    mi da un errore che non capisco..

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -3,13 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Comic;
 
 class ComicController extends Controller
 {
     public  function caratteri($id) {
-        $data = [
-            'title' => 'pagina CHARACTERS'
-        ];
+        // senza compact
+        // $data = [
+        //     'title' => 'pagina CHARACTERS'
+        // ];
+        $title = Comic::all();
+        
         return view('CHARACTERS', $data);
     }
 
@@ -41,3 +45,44 @@ class ComicController extends Controller
         return view('FANS');
     }
 }
+
+
+// Soluzione senza Model
+
+// class ComicController extends Controller
+// {
+//     public  function caratteri($id) {
+//         $data = [
+//             'title' => 'pagina CHARACTERS'
+//         ];
+//         return view('CHARACTERS', $data);
+//     }
+
+//     public  function fumetti() {
+//         return view('COMICS');
+//     }
+
+//     public  function film() {
+//         return view('MOVIES');
+//     }
+
+//     public  function vision() {
+//         return view('TV');
+//     }
+
+//     public  function giochi() {
+//         return view('GAMES');
+//     }
+
+//     public  function meme() {
+//         return view('COLLECTIBLES');
+//     }
+
+//     public  function video() {
+//         return view('VIDEOS');
+//     }
+
+//     public  function fans() {
+//         return view('FANS');
+//     }
+// }
